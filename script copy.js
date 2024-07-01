156faed01f5e50dbac257ae838bcfce43701bf43
essebr <153366953+essebr@users.noreply.github.com>
2024-07-01 21:15:41 +0700

    bigchange1

--- script copy.js
+++
@@ -1,7 +0,0 @@
-const header=document.querySelector("header");
-const scroll_btn=document.querySelector("#scroll");
-
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY > 10);
-    scroll_btn.classList.toggle("active",window.scrollY > 50);
-});
