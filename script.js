bce6ea038d10ab34c25a4777c6323200e18a2e1e
essebr <153366953+essebr@users.noreply.github.com>
2024-07-06 19:33:53 +0700

    e

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY > 10);
     scroll_btn.classList.toggle("active",window.scrollY > 50);
 });
+
+// // awarded
+// const data = null;
+
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener('readystatechange', function () {
+// 	if (this.readyState === this.DONE) {
+// 		console.log(this.responseText);
+// 	}
+// });
+
+// xhr.open('GET', 'https://hapi-books.p.rapidapi.com/top/2021');
+// xhr.setRequestHeader('x-rapidapi-key', 'fec6413180mshfcbe065ba7cfe85p1e3d61jsn1647ff627775');
+// xhr.setRequestHeader('x-rapidapi-host', 'hapi-books.p.rapidapi.com');
+
+// xhr.send(data);
+// // info
+// const data2 = null;
+
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener('readystatechange', function () {
+// 	if (this.readyState === this.DONE) {
+// 		console.log(this.responseText);
+// 	}
+// });
+
+// xhr.open('GET', 'https://hapi-books.p.rapidapi.com/book/56597885');
+// xhr.setRequestHeader('x-rapidapi-key', 'fec6413180mshfcbe065ba7cfe85p1e3d61jsn1647ff627775');
+// xhr.setRequestHeader('x-rapidapi-host', 'hapi-books.p.rapidapi.com');
+
+// xhr.send(data);
